cfb0467b45c67cefe1299107e5eb42a84431dcdc
LeeYuRi1 <dbf51561590@gmail.com>
2019-06-14 00:51:42 +0900

    init project

---
+++ src/main/java/com/data/dataproject/DataProjectApplication.java
@@ -0,0 +1,13 @@
+package com.data.dataproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataProjectApplication.class, args);
+    }
+
+}
