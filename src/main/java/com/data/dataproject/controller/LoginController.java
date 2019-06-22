5c65f7a61a0495becab705357e72c226a8933048
LeeYuRi1 <dbf51561590@gmail.com>
2019-06-22 15:34:54 +0900

    swagger 추가

--- src/main/java/com/data/dataproject/controller/LoginController.java
+++ src/main/java/com/data/dataproject/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.data.dataproject.controller;
 import com.data.dataproject.dto.TokenDto;
 import com.data.dataproject.model.DefaultRes;
 import com.data.dataproject.service.AuthService;
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @ControllerAdvice
 @RestController
 @RequiredArgsConstructor
+@Api(value = "LoginController")
 public class LoginController {
 
     private static final DefaultRes FAIL_DEFAULT_RES = new DefaultRes(500, "서버 내부 에러");
